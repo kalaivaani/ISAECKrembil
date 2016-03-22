2f0d67652a5e3fac310c3df3f9bf7d29b4c849b8
Kala Sundararajan <ksundara@ads.uhnresearch.ca>
2016-03-22 12:59:20 -0400

    - cleaned up analysis vars and subsets - added regression models and outputs: clinical variables, clinical step-wise, all variables, all step-wise
    Signed-off-by: Kala Sundararajan <ksundara@ads.uhnresearch.ca>

--- scripts/analysis_regression.R
+++ scripts/analysis_regression.R
@@ -12,24 +12,21 @@ ISAEC <- ISAEC_cons
 # restrict sample to ones with self-efficacy scores
 ISAEC_SE <- subset(ISAEC, !is.na(ISAEC$BASELINE_SELF_EFFICACY_SCORE))
 
-analysis_vars <- c("GENDER", "AGE","COLLECTION_CENTER", "BPDURATION", "LPDURATION",
-                   "BASELINE_BPREST", "BASELINE_BPACTIVITY", "BASELINE_LEGPREST", 
-                   "BASELINE_LEGPACTIVITY", "BASELINE_TOTAL", "BASELINE_RESULT", "SMOKETYPE",
-                   "FREQ_EXERC", "BPLOC","BPSTOP","BPHX","BPDURATION","LPDURATION","CLAIM","NONRADIC","PAINPERC",
-                   "BOWELCHANGE","LEGWEAK","LEGNUMB","WORK_UNDEREMP","MEDOPI","EXPECTRELIEF3",
-                   "EXPECTACT3","EXPECTSLEEP3","EXPECTWORK3","EXPECTREC3","EXPECTPREV3")
+analysis_vars <- c("AGE","BASELINE_BPREST", "BASELINE_BPACTIVITY", "BASELINE_LEGPREST", "BASELINE_LEGPACTIVITY",
+                   "BASELINE_TOTAL", "CMCOUNT","GENDER", "SMOKETYPE", "FREQ_EXERC", "BPLOC", "BPSTOP", "BPHX", 
+                   "BPDURATION", "LPDURATION", "NONRADIC", "BOWELCHANGE", "LEGWEAK", "LEGNUMB", "MEDOPI",
+                   "BASELINE_RESILIENCE", "BASELINE_RESULT", "CLAIM", "WORK_UNDEREMP", "WORKSATIS", 
+                   "WORKPHYS", "WORKMENT", grep("^EXPECT.*3$", names(ISAEC), value=TRUE), "COLLECTION_CENTER")
 
 # restrict to complete cases
-ISAEC_SEreg <- ISAEC_SE[,c("BASELINE_SELF_EFFICACY_SCORE",analysis_vars)]
-ISAEC_SEreg.nm <- na.omit(ISAEC_SEreg)
+ISAEC_SEreg <- na.omit(ISAEC_SE[,c("BASELINE_SELF_EFFICACY_SCORE",analysis_vars)])
 
 # complete cases without pain ratings
 nprs_vars <- names(ISAEC_SEreg) %in% c("BASELINE_BPREST", "BASELINE_BPACTIVITY", "BASELINE_LEGPREST", "BASELINE_LEGPACTIVITY")
-ISAEC_SEreg_rmNPRS <- ISAEC_SEreg[,!nprs_vars]
-ISAEC_SEreg_rmNPRS.nm <- na.omit(ISAEC_SEreg_rmNPRS)
+ISAEC_SEreg_rmNPRS <- na.omit(ISAEC_SEreg[,!nprs_vars])
 
 # full model, including NPRS scores
-SE_fmNPRS <- with(ISAEC_SEreg.nm, lm(BASELINE_SELF_EFFICACY_SCORE ~ GENDER + AGE + COLLECTION_CENTER + 
+SE_fmNPRS <- with(ISAEC_SEreg, lm(BASELINE_SELF_EFFICACY_SCORE ~ GENDER + AGE + COLLECTION_CENTER + 
                                        factor(BPDURATION) + factor(LPDURATION) +
                                        BASELINE_BPREST + BASELINE_BPACTIVITY + BASELINE_LEGPREST + 
                                        BASELINE_LEGPACTIVITY + BASELINE_TOTAL + relevel(factor(BASELINE_RESULT), ref="Low risk") +
@@ -39,7 +36,7 @@ SE_fmNPRS <- with(ISAEC_SEreg.nm, lm(BASELINE_SELF_EFFICACY_SCORE ~ GENDER + AGE
                                        factor(EXPECTPREV3)))
 
 # model without NPRS scores
-SE_fm_exNPRS <- with(ISAEC_SEreg.nm, lm(BASELINE_SELF_EFFICACY_SCORE ~ GENDER + AGE + COLLECTION_CENTER + 
+SE_fm_exNPRS <- with(ISAEC_SEreg, lm(BASELINE_SELF_EFFICACY_SCORE ~ GENDER + AGE + COLLECTION_CENTER + 
                                           factor(BPDURATION) + factor(LPDURATION) +
                                           BASELINE_TOTAL + relevel(factor(BASELINE_RESULT), ref="Low risk") +
                                           factor(MARITAL) + factor(SMOKETYPE) + BOWELCHANGE + LEGWEAK + LEGNUMB + 
@@ -51,3 +48,46 @@ lapply(list(withNPRS = SE_fmNPRS, noNPRS = SE_fm_exNPRS), summary)
 step_SEmodel <- stepAIC(SE_fmNPRS, direction="both")
 
 
+############## clinical characteristics ##################
+
+clin_vars_cont <- c("AGE","BASELINE_BPREST", "BASELINE_BPACTIVITY", "BASELINE_LEGPREST", "BASELINE_LEGPACTIVITY",
+               "BASELINE_TOTAL", "CMCOUNT")
+clin_vars_cat <- c("GENDER", "SMOKETYPE", "FREQ_EXERC", "BPLOC", "BPSTOP", "BPHX", "BPDURATION", "LPDURATION", 
+                   "NONRADIC", "BOWELCHANGE", "LEGWEAK", "LEGNUMB", "MEDOPI")
+
+clin_formula <- paste0("BASELINE_SELF_EFFICACY_SCORE ~ ", 
+                          paste(clin_vars_cont, collapse=" + "), " + ", 
+                          paste(paste0("factor(", clin_vars_cat, ")"), collapse=" + ")
+                      )
+
+SE_clin <- lm(as.formula(clin_formula), data=ISAEC_SEreg)
+SE_clin_output <- LMOut(SE_clin)
+
+#### non-clinical characteristics ####
+
+nonclin_vars_cont <- c("BASELINE_RESILIENCE")
+nonclin_vars_cat <- c("BASELINE_RESULT", "CLAIM", "WORK_UNDEREMP", "WORKSATIS", "WORKPHYS",
+                      "WORKMENT", grep("^EXPECT.*3$", names(ISAEC), value=TRUE))
+
+full_formula <- paste0(clin_formula, " + ", 
+                       paste(nonclin_vars_cont, collapse=" + "), " + ",
+                       paste(paste0("factor(", nonclin_vars_cat, ")"), collapse=" + "))
+
+SE_full <- lm(as.formula(full_formula), data=ISAEC_SEreg)
+SE_full_output <- LMOut(SE_full)
+
+#### step-wise models ####
+SE_clin_step <- stepAIC(SE_clin, direction="both")
+SE_full_step <- stepAIC(SE_full, direction="both")
+
+
+clin_model_outputs <- merge(LMOut(SE_clin), LMOut(SE_clin_step), by="row.names", all=TRUE)
+full_model_outputs <- merge(LMOut(SE_full), LMOut(SE_full_step), by="row.names", all=TRUE)
+all_model_outputs <- merge(clin_model_outputs, full_model_outputs, by="Row.names", all=TRUE)
+all_model_outputs <- all_model_outputs[match(row.names(LMOut(SE_full)),all_model_outputs$Row.names),]
+write.csv(all_model_outputs, file="output/reg_models.csv", na="", row.names=FALSE)
+
+
+fit_stats <- do.call(rbind, lapply(list(clin=SE_clin, clin.step=SE_clin_step, full=SE_full, full.step=SE_full_step), 
+                                   reg_chars))
+write.csv(fit_stats, file="output/reg_fit_stats.csv")
