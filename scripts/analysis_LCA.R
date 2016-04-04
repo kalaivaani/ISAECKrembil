6a3be8f6fb8a83d1289f87a3317343250314ea07
Kala Sundararajan <ksundara@ads.uhnresearch.ca>
2016-04-04 11:28:42 -0400

    - Latent class analysis script: added variable lists

--- scripts/analysis_LCA.R
+++ scripts/analysis_LCA.R
@@ -1,10 +1,13 @@
 ################################### Project: analyses for ISAEC Krembil abstracts #####################################
-# date: 2016-03-17
+# date: 2016-03-23
 # written by: Kala Sundararajan
 # BL and 6 month data from ISAEC clinical database, and consent status from master list
 
 
-########################### analysis_regression.R: self-efficacy vs baseline characteristics ###############################
+########################### analysis_LCA.R: self-efficacy vs baseline characteristics ###############################
+
+# set seed
+set.seed(38947)
 
 load(file="processed_data/ISAEC_cons_newvars.Rdata")
 ISAEC <- ISAEC_cons
@@ -12,65 +15,61 @@ ISAEC <- ISAEC_cons
 # restrict sample to ones with self-efficacy scores
 ISAEC_SE <- subset(ISAEC, !is.na(ISAEC$BASELINE_SELF_EFFICACY_SCORE))
 
-analysis_vars <- c("GENDER", "AGE","COLLECTION_CENTER", "BPDURATION", "LPDURATION",
-                   "BASELINE_BPREST", "BASELINE_BPACTIVITY", "BASELINE_LEGPREST", 
-                   "BASELINE_LEGPACTIVITY", "BASELINE_TOTAL", "BASELINE_RESULT", "BASELINE_RESILIENCE", "MARITAL","SMOKETYPE",
-                   "FREQ_EXERC", "BPLOC","BPSTOP","BPHX","BPDURATION","LPDURATION","CLAIM","NONRADIC","PAINPERC",
-                   "BOWELCHANGE","LEGWEAK","LEGNUMB","WORK_UNDEREMP","MEDOPI","EXPECTRELIEF3",
-                   "EXPECTACT3","EXPECTSLEEP3","EXPECTWORK3","EXPECTREC3","EXPECTPREV3")
+
+clin_vars_cont <- c("AGE","BASELINE_BPREST", "BASELINE_BPACTIVITY", "BASELINE_LEGPREST", "BASELINE_LEGPACTIVITY",
+                    "BASELINE_TOTAL", "CMCOUNT")
+clin_vars_cat <- c("GENDER", "SMOKETYPE", "FREQ_EXERC", "BPLOC", "BPSTOP", "BPHX", "BPDURATION", "LPDURATION", 
+                   "NONRADIC", "BOWELCHANGE", "LEGWEAK", "LEGNUMB", "MEDOPI")
+nonclin_vars_cont <- c()
+nonclin_vars_cat <- c("BASELINE_RESULT", "CLAIM", "WORK_UNDEREMP", "WORKSATIS", "WORKPHYS",
+                      "WORKMENT", grep("^EXPECT.*3$", names(ISAEC), value=TRUE))
+
+analysis_vars <- c(clin_vars_cont, clin_vars_cat, nonclin_vars_cont, nonclin_vars_cat)
 
 # restrict to complete cases
-ISAEC_SEreg <- ISAEC_SE[,c("BASELINE_SELF_EFFICACY_SCORE",analysis_vars)]
-ISAEC_SEreg.nm <- na.omit(ISAEC_SEreg)
+ISAEC_SE <- na.omit(ISAEC_SE[,c("BASELINE_SELF_EFFICACY_SCORE",analysis_vars)])
 
-# set seed
-set.seed(38947)
 
-# model
-predictors <- FLXMRglm(~ factor(BPDURATION) + factor(LPDURATION) + CLAIM + BPSTOP + 
-                         BASELINE_BPREST + BASELINE_BPACTIVITY + BASELINE_LEGPREST + 
-                         BASELINE_LEGPACTIVITY + BASELINE_TOTAL + factor(BASELINE_RESULT)
-                         BOWELCHANGE + LEGWEAK + LEGNUMB + MEDOPI + factor(EXPECTRELIEF3) + factor(EXPECTACT3) + 
-                         factor(EXPECTSLEEP3) + factor(EXPECTWORK3) + factor(EXPECTREC3) + 
-                         factor(EXPECTPREV3) + GENDER + AGE + COLLECTION_CENTER + factor(MARITAL) + factor(SMOKETYPE) + 
-                         WORK_UNDEREMP + FREQ_EXERC + NONRADIC + PAINPERC)
+# create formulas
+clin_formula <- paste0("BASELINE_SELF_EFFICACY_SCORE ~ ", 
+                       paste(clin_vars_cont, collapse=" + "), " + ", 
+                       paste(paste0("factor(", clin_vars_cat, ")"), collapse=" + ")
+                        )
 
-clin_predictors <- FLXMRglm(~ factor(BPDURATION) + factor(LPDURATION) + BPSTOP + 
-                              BASELINE_BPREST + BASELINE_BPACTIVITY + BASELINE_LEGPREST + 
-                              BASELINE_LEGPACTIVITY +
-                              BOWELCHANGE + LEGWEAK + LEGNUMB + MEDOPI + GENDER + AGE + factor(SMOKETYPE))
-LC_SEmodel <- stepFlexmix(BASELINE_SELF_EFFICACY_SCORE~1, model=predictors, data=ISAEC_SEreg.nm, k=1:5)
+full_formula <- paste0(clin_formula, " + ", 
+                       paste(nonclin_vars_cont, collapse=" + "), " + ",
+                       paste(paste0("factor(", nonclin_vars_cat, ")"), collapse=" + "))
+# remove pain location
+clin_formula_nopainloc <- sub("factor\\(BPLOC\\) \\+ ", "", clin_formula)
+full_formula_nopainloc <- sub("factor\\(BPLOC\\) \\+ ", "", clin_formula)
 
-plot(LC_SEmodel, what="BIC")
-best_SEmodel <- getModel(LC_SEmodel, which="BIC")
+
+############# LCR models ##################
+SE_clusters <- stepFlexmix(BASELINE_SELF_EFFICACY_SCORE ~ 1, data=ISAEC_SE, k=1:5)
+plot(SE_clusters, what="BIC")
+best_SEmodel <- getModel(SE_clusters, which="BIC")
+summary(best_SEmodel)
 parameters(best_SEmodel)
-ISAEC_SEreg.nm$cluster <- best_SEmodel@cluster
-cluster_SEstats <- with(ISAEC_SEreg.nm, tapply(BASELINE_SELF_EFFICACY_SCORE, cluster, function(x) {
+ISAEC_SE$cluster <- best_SEmodel@cluster
+cluster_SEstats <- with(ISAEC_SE, tapply(BASELINE_SELF_EFFICACY_SCORE, cluster, function(x) {
   c(mean=mean(x), sd=sd(x), count=length(x))
 }))
+ggplot(ISAEC_SE, aes(x=BASELINE_SELF_EFFICACY_SCORE)) +
+  geom_histogram(aes(fill=factor(cluster)), alpha=0.5, position="identity", bins=50) +
+  ggtitle("Two latent components of baseline self-efficacy\nin ISAEC patients") +
+  scale_fill_discrete(name="Cluster") +
+  theme_bw(base_size=20)
 
-ggplot(ISAEC_SEreg.nm, aes(y=BASELINE_SELF_EFFICACY_SCORE, x=factor(cluster))) + geom_boxplot()
-ggplot(ISAEC_SEreg.nm, aes(x=BASELINE_SELF_EFFICACY_SCORE, color=factor(cluster))) + geom_density()
-
-ggplot(ISAEC_SEreg.nm, aes(x=factor(MEDOPI), y=BASELINE_SELF_EFFICACY_SCORE)) + geom_boxplot() + facet_grid(.~cluster)
 
 
-ggplot(ISAEC_SEreg.nm, aes(x=factor(LPDURATION), y=BASELINE_SELF_EFFICACY_SCORE)) + geom_boxplot() + facet_grid(.~cluster)
+SE_clusters_bploc <- stepFlexmix(BASELINE_SELF_EFFICACY_SCORE ~ 1, concomitant=FLXPmultinom(~BPLOC), data=ISAEC_SE, k=1:5)
+plot(SE_clusters_bploc, what="BIC")
 
-stepLC_SEmodel <- stepFlexmix(BASELINE_SELF_EFFICACY_SCORE ~ GENDER + AGE + 
-                        factor(BPDURATION) + factor(LPDURATION) +
-                        BASELINE_BPREST + BASELINE_BPACTIVITY + BASELINE_LEGPREST + 
-                        BASELINE_LEGPACTIVITY + BASELINE_TOTAL + 
-                        factor(MARITAL) + factor(SMOKETYPE) + BOWELCHANGE + LEGWEAK + LEGNUMB + 
-                        WORK_UNDEREMP + MEDOPI, data=ISAEC_SEreg.nm, k=1:5, nrep=5)
 
-plot(stepLC_SEmodel, what="BIC")
-best_model <- getModel(stepLC_SEmodel, which="BIC")
-parameters(best_model)
+clin_step_model <- stepFlexmix(as.formula(clin_formula), data=ISAEC_SE, k=1:5)
+plot(clin_step_model, what="BIC")
+best_SEmodel <- getModel(clin_step_model, which="BIC")
+parameters(best_SEmodel)
 
-summary(LC_SEmodel)
-
-coef_LC_SEmodel <- refit(LC_SEmodel)
-View(LC_SEmodel@cluster)
-
-ISAEC_SEreg.nm$cl2 <- LC_SEmodel@cluster
+clin_step_model_painloc <- stepFlexmix(as.formula(clin_formula), concomitant = FLXPmultinom(~BPLOC), data=ISAEC_SE, k=1:5)
+plot(clin_step_model_painloc, what="BIC")
